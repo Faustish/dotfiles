e13b1ebbe7f0e705f5172da987c99daa54f5345a
Kjetil Carino-Faust <kjetil.carino-faust@thon.com>
2026-06-16 19:59:30 +0200

    init: add dotfiles for tmux, alacritty, nvim, zsh

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,24 @@
+  -- Bootstrap lazy.nvim
+  local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+  if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+      "git", "clone", "--filter=blob:none",
+      "https://github.com/folke/lazy.nvim.git",
+      "--branch=stable", lazypath,
+    })
+  end
+  vim.opt.rtp:prepend(lazypath)
+
+  require("lazy").setup({
+    { "folke/tokyonight.nvim", priority = 1000, config = function()
+        vim.cmd("colorscheme tokyonight-night")
+      end,
+    },
+    { "nvim-tree/nvim-tree.lua", dependencies = { "nvim-tree/nvim-web-devicons" }, config = true },
+    { "nvim-lua/plenary.nvim" },
+    { "NeogitOrg/neogit", dependencies = { "nvim-lua/plenary.nvim" }, config = true },
+  })
+
+  -- Keymaps
+  vim.opt.clipboard = "unnamedplus"
+  vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { silent = true })
